3e53fd102a4fd53c073dc9ed8e893e5ef4860f79
JorgeGutierrez <jorgeguti.0791@gmail.com>
2021-11-03 23:48:19 -0500

    validate login and register

--- src/Services/Login/index.js
+++ src/Services/Login/index.js
@@ -1,53 +1,64 @@
-import config from '../../Assets/localConfig.json';
+import config from "../../Assets/localConfig.json";
 
 const Login = (data, callback, errorCallback) => {
-    let url = config.baseApi+'user_login_account.php';
-
-    fetch(url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }, 
-        body: JSON.stringify(data)
-    })
+  let url = config.baseApi + "user_login_account.php";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
     .then((response) => {
-        //console.log(response);
-        return response.json();
+      //console.log(response);
+      return response.json();
     })
-    .then((json)=>{
-        if(json.response==='INVALID_ACCOUNT_PASSWORD'){
-            errorCallback(json.response);
-        }
-        else{
-            callback(json);
-        }
+    .then((json) => {
+      if (json.response === "INVALID_ACCOUNT_PASSWORD") {
+        errorCallback(json.response);
+      } else {
+        callback(json);
+      }
     })
     .catch((error) => {
-        console.log(error);
+      console.log(error);
     });
-}
+};
 
 const LoginFacebookGoogle = (data, callback, errorCallback) => {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "1b845f0b84f5e8d02db992788ec8e1a2be37e11a1ace1b7ca67a5fa5c23b0b30");
-    myHeaders.append("Content-Type", "application/json");
+  var myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    "1b845f0b84f5e8d02db992788ec8e1a2be37e11a1ace1b7ca67a5fa5c23b0b30"
+  );
+  myHeaders.append("Content-Type", "application/json");
 
-    var raw = JSON.stringify({"email":data.email,"nombre":data.nombre,"photo_url":null,"device_name":"Web app","tipo_login":data.tipo_login,"unique_id":data.unique_id});
+  var raw = JSON.stringify({
+    email: data.email,
+    nombre: data.nombre,
+    photo_url: null,
+    device_name: "Web app",
+    tipo_login: data.tipo_login,
+    unique_id: data.unique_id,
+  });
 
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
+  var requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  };
 
-    fetch("http://34.95.245.77/android_doctor_vet/api/user_facebook_google_login.php", requestOptions)
-    .then(response => response.json())
-    .then(result => {
-        //console.log(result);
-        callback(result);
+  fetch(
+    "http://34.95.245.77/android_doctor_vet/api/user_facebook_google_login.php",
+    requestOptions
+  )
+    .then((response) => response.json())
+    .then((result) => {
+      //console.log(result);
+      callback(result);
     })
-    .catch(error => errorCallback(error));
-}
+    .catch((error) => errorCallback(error));
+};
 
-export { Login, LoginFacebookGoogle }
+export { Login, LoginFacebookGoogle };
--- src/Services/Register/index.js
+++ src/Services/Register/index.js
@@ -1,24 +1,24 @@
-import config from '../../Assets/localConfig.json';
+import config from "../../Assets/localConfig.json";
 
 const Register = (data, callback, callBackError) => {
-    let url = config.baseApi+'user_create_account.php';
-    fetch(url,{
-        method:'POST',
-        headers:{
-            'Content-Type': 'text/plain',
-        },
-        body:JSON.stringify(data)
-    })
-    .then((resp)=>{
+  let url = config.baseApi + "user_create_account.php";
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "text/plain",
+    },
 
-        console.log(resp);
-        return resp.json();
+    body: JSON.stringify(data),
+  })
+    .then((resp) => {
+      console.log(resp);
+      return resp.json();
     })
-    .then((json)=>{
-        callback(json);
+    .then((json) => {
+      callback(json);
     })
-    .catch((error)=>{
-        callBackError(error)
-    })
-}
-export {Register}
+    .catch((error) => {
+      callBackError(error);
+    });
+};
+export { Register };
